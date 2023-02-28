6ae0bc22c07c198a9e50a60cf3c5624474412943
vikrantdb06 <123267438+vikrantdb06@users.noreply.github.com>
2023-02-28 20:52:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
   ami           = "ami-065bb5126e4504910"
   instance_type = "t2.micro"
   tags = {
-    Name = "example-instance"
+    Name = "example1-instance"
   }
 }
